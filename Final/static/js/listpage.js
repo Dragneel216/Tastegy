3de04735cee4a0b517f2a517307896673332f11c
Nithin Cimu <133194108+NitCimu26@users.noreply.github.com>
2024-05-16 22:12:58 +0530

    sorting done

--- Final/static/js/listpage.js
+++ Final/static/js/listpage.js
@@ -2,12 +2,19 @@ function goBack() {
     window.history.back();
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Call sortRecipes to apply the default sorting when the page loads
+    sortRecipes();
+});
+
 function sortRecipes() {
     const sortOption = document.getElementById('sort').value;
     const recipeContainer = document.getElementById('recipeContainer');
     const recipes = Array.from(recipeContainer.getElementsByClassName('recipe-card'));
 
-    if (sortOption === 'TotalTimeInMinsAsc') {
+    if (sortOption === 'default') {
+        recipes.sort((a, b) => parseInt(a.getAttribute('data-index-no')) - parseInt(b.getAttribute('data-index-no')));
+    } else if (sortOption === 'TotalTimeInMinsAsc') {
         recipes.sort((a, b) => parseInt(a.getAttribute('data-total-time')) - parseInt(b.getAttribute('data-total-time')));
     } else if (sortOption === 'TotalTimeInMinsDesc') {
         recipes.sort((a, b) => parseInt(b.getAttribute('data-total-time')) - parseInt(a.getAttribute('data-total-time')));
